d75f1840bed46acfdee1fb03871a09df5065bd21
ardabagce <ardabagce@gmail.com>
2022-12-30 14:27:23 +0300

    Navigation, router and some small add-ons

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Contact from "./Pages/Contact";
+import Resume from "./Pages/Resume";
+import NavigationBar from "./Components/Navbar";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate
+} from "react-router-dom";
+import ScrollToTop from "./Components/ScrollToTop";
+
 function App() {
   return (
-    <div>
-      
+    <Router>
+    <div className="App">
+      <ScrollToTop />
+      <NavigationBar/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/resume" element={<Resume />} />
+        <Route path="*" element={<Navigate to="/"/>} />
+      </Routes>
     </div>
+  </Router>
   );
 }
 
---
+++ src/Pages/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import HomeHero from "../Components/HomeHero"
+
+export class Home extends Component {
+  render() {
+    return (
+        <div>  
+            <HomeHero/>
+        </div>
+    )
+  }
+}
+
+export default Home
